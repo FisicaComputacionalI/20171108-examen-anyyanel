c3ac7bd65469b401bdd7e45a5fcec7a59886b391
anyyanel <31254545+anyyanel@users.noreply.github.com>
2017-11-10 13:30:39 -0600

    Add files via upload
    Programa tres de examen ordinario 
    Any Yanel Morales Guevara

---
+++ programatres.cc
@@ -0,0 +1,19 @@
+//Any Yanel Morales Guevara
+//Programa tres para examen ordinario
+//10/11/17
+#include <iostream>
+using namespace std;
+int main ()
+{
+  float d;
+  float t;
+  float v;
+  cout<<"Programa para calcular la velocidad de un cuerpo"<<endl;
+  cout<<"Inserte la distancia que recorre el cuerpo"<<endl;
+  cin>>d;
+  cout<<"Inserte el tiempo que tomó en recorrer dicha distancia"<<endl;
+  cin>>t;
+  v=d/t;
+  cout<<"La velocidad es: "<< v<<endl;
+  return 0;
+}
